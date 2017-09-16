952914d8babce34f5182dd21a5c2d3892f9b92b6
Danilyk <Danilko_01@mail.ru>
2017-09-16 23:44:21 +0700

    delete connect.php

--- connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $connect_db = mysql_connect("localhost","ci48734_test","qwerty") or die(mysql_error());
-    $db = "ci48734_test";
-    if ( !$connect_db ) die ("Невозможно подключение к MySQL");
-    mysql_select_db ( $db ) or die ("Невозможно открыть $db");
-?>
